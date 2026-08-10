f97ceecdba32908709f4e114b17fc7c751d59a4a
Minecrafter!! <159472635+483aa@users.noreply.github.com>
2026-08-10 18:50:44 +0500

    Add files via upload

---
+++ hvll/levels.js
@@ -0,0 +1,482 @@
+const mainListData = [
+  {
+    "id": 127323087,
+    "name": "Society",
+    "author": "Neomarbilan",
+    "difficulty": "wPopoff",
+    "image": "https://www.youtube.com/watch?v=3CoEaH1CM7o",
+    "isNew": false
+  },
+  {
+    "id": 119550490,
+    "name": "Amethyst",
+    "author": "iMist",
+    "difficulty": "wPopoff",
+    "image": "https://www.youtube.com/watch?v=4lfkzz1VCbA",
+    "isNew": false
+  },
+  {
+    "id": 119544028,
+    "name": "Thinking Space II",
+    "author": "CairoX",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=CELNmHwln_c",
+    "isNew": false
+  },
+  {
+    "id": 126242564,
+    "name": "Flamewall",
+    "author": "UNarwall",
+    "difficulty": "CuatrocientosYT",
+    "image": "https://www.youtube.com/watch?v=x4Io4zkWVRw",
+    "isNew": false
+  },
+  {
+    "id": 86407629,
+    "name": "Tidal Wave",
+    "author": "OniLinkGD",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=9fsZ014qB3s",
+    "isNew": false
+  },
+  {
+    "id": 142896409,
+    "name": "Green Bullet",
+    "author": "cherryteam",
+    "difficulty": "Nickname",
+    "image": "https://www.youtube.com/watch?v=JG-qcvVkZFw",
+    "isNew": false
+  },
+  {
+    "id": 133175713,
+    "name": "ORBIT",
+    "author": "MindCap",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=QKcv8DkNPd0",
+    "isNew": false
+  },
+  {
+    "id": 109780665,
+    "name": "Nullscapes",
+    "author": "ItzKiba",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=EztneTPp5CU",
+    "isNew": false
+  },
+  {
+    "id": 110816181,
+    "name": "BOOBAWAMBA",
+    "author": "Akunakunn",
+    "difficulty": "eastshark",
+    "image": "https://www.youtube.com/watch?v=20fYiqLAo_E",
+    "isNew": false
+  },
+  {
+    "id": 609629,
+    "name": "SARY NEVER CLEAR",
+    "author": "Optic Sunflow",
+    "difficulty": "Glowgmd",
+    "image": "https://www.youtube.com/watch?v=Hpo77uVq75M",
+    "isNew": false
+  },
+  {
+    "id": 117692518,
+    "name": "Quanteuse processing",
+    "author": "Renn241",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=j5NC0u1Q91Q",
+    "isNew": false
+  },
+  {
+    "id": 137886942,
+    "name": "Lesopowal",
+    "author": "cherryteam",
+    "difficulty": "Bacon",
+    "image": "https://www.youtube.com/watch?v=RTXnFHkGO_g",
+    "isNew": false
+  },
+  {
+    "id": 10887708,
+    "name": "Sakupen circles",
+    "author": "Nick24",
+    "difficulty": "ad3usgmd",
+    "image": "https://www.youtube.com/watch?v=AdrmZsy52EY",
+    "isNew": false
+  },
+  {
+    "id": 135661111,
+    "name": "The Bloop",
+    "author": "murkkat",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=ROuXHSKEhHA",
+    "isNew": false
+  },
+  {
+    "id": 136742562,
+    "name": "Old Orochi",
+    "author": "Platnuu",
+    "difficulty": "SeptaGon7",
+    "image": "https://www.youtube.com/watch?v=YxtFAQtLYQY",
+    "isNew": false
+  },
+  {
+    "id": 144988390,
+    "name": "Antarctic Lights",
+    "author": "declanlc",
+    "difficulty": "Axinity",
+    "image": "https://www.youtube.com/watch?v=Uue4ZRhVf-I",
+    "isNew": false
+  },
+  {
+    "id": 62912799,
+    "name": "Ashley Wave Trials",
+    "author": "OddMod",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=aTxt76U3e2Q",
+    "isNew": false
+  },
+  {
+    "id": 4125776,
+    "name": "Silent clubstep",
+    "author": "TheRealSilent",
+    "difficulty": "zoe",
+    "image": "https://www.youtube.com/watch?v=GR4OMkS3SN8",
+    "isNew": false
+  },
+  {
+    "id": 116174063,
+    "name": "Every End",
+    "author": "MindCap",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=ZyRmgwSh5tQ",
+    "isNew": false
+  },
+  {
+    "id": 134942736,
+    "name": "Penumbral",
+    "author": "cherryteam",
+    "difficulty": "w3rty",
+    "image": "https://www.youtube.com/watch?v=nVHPFf13M-I",
+    "isNew": false
+  },
+  {
+    "id": 138704906,
+    "name": "Ultra Paracosm",
+    "author": "declanlc",
+    "difficulty": "wity26",
+    "image": "https://www.youtube.com/watch?v=4V24spYQWuE",
+    "isNew": false
+  },
+  {
+    "id": 127997391,
+    "name": "Subsuming Vortex",
+    "author": "SeptaGon7",
+    "difficulty": "Cursed",
+    "image": "https://www.youtube.com/watch?v=0eYG1ogJpIQ",
+    "isNew": false
+  },
+  {
+    "id": 114283297,
+    "name": "andromeda",
+    "author": "Insxne97",
+    "difficulty": "Taiago",
+    "image": "https://www.youtube.com/watch?v=mk3TDemdkC0",
+    "isNew": false
+  },
+  {
+    "id": 146538900,
+    "name": "SANGUINE",
+    "author": "ItsMeGeoGD",
+    "difficulty": "spookkd",
+    "image": "https://www.youtube.com/watch?v=cBVlGzobmsA",
+    "isNew": true
+  },
+  {
+    "id": 138781722,
+    "name": "KILLSTEALER",
+    "author": "Rynoxiouss",
+    "difficulty": "kloone",
+    "image": "https://www.youtube.com/watch?v=uhH4oqWrsfo",
+    "isNew": false
+  },
+  {
+    "id": 142529115,
+    "name": "Herr Machine",
+    "author": "herrteam",
+    "difficulty": "BONN GD",
+    "image": "https://www.youtube.com/watch?v=CBcQqmqWFo4",
+    "isNew": false
+  },
+  {
+    "id": 112313819,
+    "name": "Anathema",
+    "author": "nikroplays",
+    "difficulty": "Whizkid05",
+    "image": "https://www.youtube.com/watch?v=_uKwmjHmySI",
+    "isNew": false
+  },
+  {
+    "id": 89496627,
+    "name": "Avernus",
+    "author": "PockeWindfish",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=16Zh8jssanc",
+    "isNew": false
+  },
+  {
+    "id": 77331805,
+    "name": "Aquamarine",
+    "author": "sincos",
+    "difficulty": "seels",
+    "image": "https://www.youtube.com/watch?v=Q__EnRojGt4",
+    "isNew": false
+  },
+  {
+    "id": 88686539,
+    "name": "Bonfire",
+    "author": "Jakalope",
+    "difficulty": "vvay",
+    "image": "https://www.youtube.com/watch?v=7ZFoCGqCq3Y",
+    "isNew": true
+  },
+  {
+    "id": 110815379,
+    "name": "Spectre",
+    "author": "xander556",
+    "difficulty": "PersonHuman42",
+    "image": "https://www.youtube.com/watch?v=MzsSLKJrLSI",
+    "isNew": false
+  },
+  {
+    "id": 83762306,
+    "name": "roses and flowers",
+    "author": "IcEDCave",
+    "difficulty": "wPopoff",
+    "image": "https://www.youtube.com/watch?v=5QeIp5fC1hE",
+    "isNew": false
+  },
+  {
+    "id": 133065676,
+    "name": "Silent RealistiK 22",
+    "author": "gstellar",
+    "difficulty": "apu",
+    "image": "https://www.youtube.com/watch?v=jI4AySdFlIo",
+    "isNew": false
+  },
+  {
+    "id": 107805281,
+    "name": "Menace",
+    "author": "ArraegenGD",
+    "difficulty": "Whizkid05",
+    "image": "https://www.youtube.com/watch?v=nnkgghxxsEE",
+    "isNew": false
+  },
+  {
+    "id": 49896559,
+    "name": "Abyss of Darkness",
+    "author": "Exen",
+    "difficulty": "Cursed",
+    "image": "https://www.youtube.com/watch?v=ejJkpqcMMCY",
+    "isNew": false
+  },
+  {
+    "id": 76680844,
+    "name": "Old Abyss of Darkness",
+    "author": "Exen",
+    "difficulty": "Dils thik",
+    "image": "https://www.youtube.com/watch?v=RWBEfS4mLbw",
+    "isNew": false
+  },
+  {
+    "id": 73667628,
+    "name": "Acheron",
+    "author": "ryamu",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=sBKR6aUorzA",
+    "isNew": false
+  },
+  {
+    "id": 120012581,
+    "name": "Defeated Circles",
+    "author": "GXQ",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=nU5AQPzd2YA",
+    "isNew": false
+  },
+  {
+    "id": 145709435,
+    "name": "FOREST OF SPIKES",
+    "author": "Bimms",
+    "difficulty": "Brian Cheese",
+    "image": "https://www.youtube.com/watch?v=WJYh3dW8Bfo",
+    "isNew": true
+  },
+  {
+    "id": 129774598,
+    "name": "Sonic Wave Infinity",
+    "author": "Slithium",
+    "difficulty": "HSW",
+    "image": "https://www.youtube.com/watch?v=av5D0_M6hso",
+    "isNew": false
+  },
+  {
+    "id": 91351939,
+    "name": "Tunnel of Despair",
+    "author": "Exen",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=LpS4JREhW98",
+    "isNew": false
+  },
+  {
+    "id": 144560194,
+    "name": "METROPOLIS",
+    "author": "KZLR",
+    "difficulty": "Ascended",
+    "image": "https://www.youtube.com/watch?v=JtWMtMFdK24",
+    "isNew": false
+  },
+  {
+    "id": 86018142,
+    "name": "Kyouki",
+    "author": "Demishio",
+    "difficulty": "Demishio",
+    "image": "https://www.youtube.com/watch?v=KDa5c0CJTHs",
+    "isNew": false
+  },
+  {
+    "id": 70744720,
+    "name": "Subconscious",
+    "author": "Lunnaria",
+    "difficulty": "apu",
+    "image": "https://www.youtube.com/watch?v=lBXO2CTEMfs",
+    "isNew": false
+  },
+  {
+    "id": 110534288,
+    "name": "Based After Based",
+    "author": "Akunakunn",
+    "difficulty": "DiamondSplash",
+    "image": "https://www.youtube.com/watch?v=yQBFyUvB3lY",
+    "isNew": false
+  },
+  {
+    "id": 146479460,
+    "name": "Evolution",
+    "author": "strikergd10",
+    "difficulty": "Striker",
+    "image": "https://www.youtube.com/watch?v=qZaNZygvOxc",
+    "isNew": true
+  },
+  {
+    "id": 132854166,
+    "name": "Thinking Space II",
+    "author": "CoCyMain",
+    "difficulty": "Zeronium",
+    "image": "https://www.youtube.com/watch?v=RJedeB2wHCA",
+    "isNew": false
+  },
+  {
+    "id": 113599729,
+    "name": "Subterminal Point",
+    "author": "SyQual",
+    "difficulty": "PoCle",
+    "image": "https://www.youtube.com/watch?v=h2wmRMgACH4",
+    "isNew": false
+  },
+  {
+    "id": 114281093,
+    "name": "CHIL",
+    "author": "McCoco",
+    "difficulty": "McCoco",
+    "image": "https://www.youtube.com/watch?v=DROMiCc2ZRM",
+    "isNew": false
+  },
+  {
+    "id": 61775814,
+    "name": "Element 111 Rg",
+    "author": "DarkX",
+    "difficulty": "zoe",
+    "image": "https://www.youtube.com/watch?v=ZcdiTGuIJ1k",
+    "isNew": false
+  },
+  {
+    "id": 27690100,
+    "name": "Slaughterhouse",
+    "author": "IcEDCave",
+    "difficulty": "Doggie",
+    "image": "https://www.youtube.com/watch?v=kpcF1-QAHQc",
+    "isNew": false
+  },
+  {
+    "id": 54441585,
+    "name": "Unnerfed Zodiac",
+    "author": "xander556",
+    "difficulty": "nebnoob",
+    "image": "https://www.youtube.com/watch?v=3mvddPaNklg",
+    "isNew": false
+  },
+  {
+    "id": 87665224,
+    "name": "KOCMOC",
+    "author": "cherryteam",
+    "difficulty": "Zoink",
+    "image": "https://www.youtube.com/watch?v=2CxE-UWCIG4",
+    "isNew": false
+  },
+  {
+    "id": 143909846,
+    "name": "Shadow Of The Past",
+    "author": "LViZeris",
+    "difficulty": "LennyFeus",
+    "image": "https://www.youtube.com/watch?v=LEyo0LJV8os",
+    "isNew": false
+  },
+  {
+    "id": 91736575,
+    "name": "Map of Problematique",
+    "author": "Yossarian",
+    "difficulty": "seels",
+    "image": "https://www.youtube.com/watch?v=Sm0UvvwOE08",
+    "isNew": false
+  },
+  {
+    "id": 134541844,
+    "name": "zorin",
+    "author": "SnowEye",
+    "difficulty": "9i",
+    "image": "https://www.youtube.com/watch?v=U19vmpenb9g",
+    "isNew": false
+  },
+  {
+    "id": 93091893,
+    "name": "Deimos",
+    "author": "ItsHybrid",
+    "difficulty": "Doggie",
+    "image": "https://www.youtube.com/watch?v=b2yHaIk5zio",
+    "isNew": false
+  },
+  {
+    "id": 75277673,
+    "name": "Firework Ice Nation",
+    "author": "TrickGMD",
+    "difficulty": "Midgatal",
+    "image": "https://www.youtube.com/watch?v=He3iRW6Usv4",
+    "isNew": false
+  },
+  {
+    "id": 130327668,
+    "name": "BEING HOME",
+    "author": "Turtl123",
+    "difficulty": "goobs47",
+    "image": "https://www.youtube.com/watch?v=kt7FnUzK8CY",
+    "isNew": false
+  },
+  {
+    "id": 141320499,
+    "name": "Centipede",
+    "author": "zander12",
+    "difficulty": "LordVadercraft",
+    "image": "https://www.youtube.com/watch?v=UFpfgXBQzhY",
+    "isNew": false
+  },
+]
